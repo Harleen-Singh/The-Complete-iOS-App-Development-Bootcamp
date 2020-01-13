c429ce7dac6ef311303206b2f8f27f6d180f1d67
Harleen-Singh <harleensingh0001@gmail.com>
2020-01-13 11:02:34 +0530

    Add code to play sound

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -6,16 +6,30 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(key: sender.currentTitle ?? "A")
+    }
     
-
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
+
+
 
